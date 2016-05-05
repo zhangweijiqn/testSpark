87827424bc7bcf499ac9d9cec6ea72935a0e34f1
zhangwj <zhangweijian@jd.com>
2016-05-05 18:37:41 +0800

    test combineByKey function

--- src/main/scala/MlLibTest/test_Hello/Mytest.scala
+++ src/main/scala/MlLibTest/test_Hello/Mytest.scala
@@ -33,8 +33,63 @@ object Mytest {
   }
 
 
+  def testCombineByKey(): Unit ={
+
+    /*val createCombiner = (v: Double) => {
+      val queue = new BoundedPriorityQueue[Double](100);
+      queue+=(v)
+      queue
+    }
+
+    val mergeValue = (q:BoundedPriorityQueue[Double],v: (Long,Double)) => {
+      q += v._2
+    }
+    val mergeCombiners = (q1: BoundedPriorityQueue[Double], q2:BoundedPriorityQueue[Double]) => {
+      q1.++=(q2)
+      q1
+    }
+
+    val testData = sc.parallelize(Seq((1L,0.1),(1L,0.2),(1L,0.3),(2L,0.2),(2L,0.5)))
+    testData.combineByKey(createCombiner,mergeValue,mergeCombiners,10)*/
+    var rdd1 = sc.makeRDD(Array(("A",1),("A",20),("A",3),("A",100),("A",200),("A",3),("B",1),("B",2),("C",1),("B",1000),("B",802),("B",600),("C",601)))
+    val rdd2 = rdd1.combineByKey((v : Int) => v + "_",
+      (c : String, v : Int) => c + "@" + v,
+      (c1 : String, c2 : String) => c1 + "$" + c2)
+    rdd2.collect.foreach(println)
+
+    val rdd3 = rdd1.combineByKey(
+      (v : Int) => List(v),
+      (c : List[Int], v : Int) => v :: c,
+      (c1 : List[Int], c2 : List[Int]) => c1 ::: c2
+    )
+    rdd3.collect.foreach(println)
+
+    val createCombiner = (v: Int) => {
+      val queue = new BoundedPriorityQueue[Int](2)(Ordering.Option.reverse);
+      queue+=(v)
+      queue
+    }
+
+    val mergeValue = (q:BoundedPriorityQueue[Int],v: (Int)) => {
+      q += v
+    }
+    val mergeCombiners = (q1: BoundedPriorityQueue[Int], q2:BoundedPriorityQueue[Int]) => {
+      q1.++=(q2)
+      q1
+    }
+    val rdd4 = rdd1.combineByKey(
+      createCombiner,
+      mergeValue,
+      mergeCombiners,
+    10
+    )
+    println("-------------------------------")
+    rdd4.collect().foreach(x=>{println(x._1+":"+x._2.toArray.mkString(","))})
+  }
+
   def main(args:Array[String]) {
 //    testGetRDDVector
-    testRDDCartesian
+//    testRDDCartesian
+    testCombineByKey()
   }
 }
