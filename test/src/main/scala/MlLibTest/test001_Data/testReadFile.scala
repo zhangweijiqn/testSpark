07eb67ef0dd4487619927db7dd4bc81cd9eae641
zhangweijian01 <zhangweijiqn@163.com>
2017-01-13 17:21:50 +0800

    read files according to date

--- test/src/main/scala/MlLibTest/test001_Data/testReadFile.scala
+++ test/src/main/scala/MlLibTest/test001_Data/testReadFile.scala
@@ -13,4 +13,9 @@ object testReadFile {
     val data = sc.textFile("test/src/main/resources/data/train.txt,test/src/main/resources/data/train.txt") //两个文件
     println(data.count())
   }
+
+  /*读取带有日期的文件
+    1，定义一个path:String，从起始日期开始遍历到结束日期，每次构建一个日期文件路径加到path中，最终path含有多个文件
+    2，像按月份选择，可以直接使用 "/path/file_201612*,/path/file_201701*"
+  */
 }
